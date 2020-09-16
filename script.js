ee6726a2a99809700d8b1ed89c2735d791a40075
Jack Meier <jackson.meier423@gmail.com>
2020-09-15 21:43:31 -0700

    added code to calculate how much each person should pay for a meal

--- script.js
+++ script.js
@@ -3,21 +3,24 @@ let totalEl = document.querySelector("#meal-price");
 let submitEl = document
   .querySelector("#submit")
   .addEventListener("click", handleCalculateTip);
-// let splitEl = ???
+let splitEl = document.querySelector("#num-people");
+let splitBtnEl = document
+  .querySelector("#split")
+  .addEventListener("click", handleSplit);
 
-// Returns tip for given total and tip %
 function calculateTip(total, tipPercentage) {
-  // calculate the dollar amount and convert to a string with two decimal places
   let roundedResult = (total * tipPercentage).toFixed(2);
   return roundedResult;
 }
 
-// Return total amount including tip
 function calculateTotal(total, tipAmount) {
   return parseFloat(total) + parseFloat(tipAmount);
 }
 
-// Handle click on calculate tip button
+function calculateSplit(grandTotal, peopleNum) {
+  return parseFloat(grandTotal) / peopleNum;
+}
+
 function handleCalculateTip(event) {
   event.preventDefault();
 
@@ -29,4 +32,17 @@ function handleCalculateTip(event) {
 
   document.getElementById("tip-amount").textContent = `$${tipAmount}`;
   document.getElementById("new-total").textContent = `$${grandTotal}`;
+}
+
+function handleSplit(event) {
+  event.preventDefault();
+
+  let total = parseFloat(totalEl.value);
+  let tipPercentage = parseFloat(tipEl.value) * 0.01;
+  let tipAmount = calculateTip(total, tipPercentage);
+  let grandTotal = calculateTotal(total, tipAmount);
+  let peopleNum = parseInt(splitEl.value);
+  let splitAmount = calculateSplit(grandTotal, peopleNum);
+
+  document.getElementById("split-total").textContent = `$${splitAmount}`;
 }
