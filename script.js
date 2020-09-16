6b8f42c2b93259037865dfca1f1a0d15f74804b1
Jack Meier <jackson.meier423@gmail.com>
2020-09-15 21:23:16 -0700

    added code to calculate tip amount and update DOM

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// use document.querySelector to get references to the
-// tip percent, total, submit, and split elements
-// var tipEl = document.querySelector("#tip-percentage");
-// var totalEl = ???
-// var submitEl = ???
-// var splitEl = ???
+let tipEl = document.querySelector("#tip-percentage");
+let totalEl = document.querySelector("#meal-price");
+let submitEl = document
+  .querySelector("#submit")
+  .addEventListener("click", handleCalculateTip);
+// let splitEl = ???
 
 // Returns tip for given total and tip %
 function calculateTip(total, tipPercentage) {
   // calculate the dollar amount and convert to a string with two decimal places
-  var roundedResult = (total * tipPercentage).toFixed(2);
+  let roundedResult = (total * tipPercentage).toFixed(2);
   return roundedResult;
 }
 
@@ -19,18 +19,13 @@ function calculateTotal(total, tipAmount) {
 
 // Handle click on calculate tip button
 function handleCalculateTip(event) {
-  // prevent the default form behavior (prevents page from reloading)
- 
+  event.preventDefault();
 
-  // get values from inputs for tip percentage and total
-  
-  // use calculateTip and calculateTotal to determine the tip amount
-  // and total amount
+  let total = parseFloat(totalEl.value);
+  let tipPercentage = parseFloat(tipEl.value) * 0.01;
 
-  // display the tip amount and total (update the DOM)
+  let tipAmount = calculateTip(total, tipPercentage);
+  let grandTotal = calculateTotal(total, tipAmount);
+
+  document.getElementById("tip-amount").textContent = `$${tipAmount}`;
 }
-
-
-// add click listener for the submitEl which runs handleCalculateTip
-
-
